e4c8bb010d41d7296aeaefb09c5ba6254cc509de
Hraban Luyat <hraban@0brg.net>
2022-08-03 12:55:02 -0400

    refactor: 🎵🇬🇪💧⌘

--- src/index.test.js
+++ src/index.test.js
@@ -13,5 +13,9 @@ a,b,c`)).toStrictEqual([['a', 'b', 'c'], ['a', 'b', 'c']]);
 
 test('find duplicates', () => {
   expect(findDuplicates([['a', 'b', 'b'], ['a', 'b', 'c']])).toStrictEqual(["a"]);
-  expect(findDuplicates([['a1', 'b', 'b'], ['a2', 'b', 'c'], ['a1', 'b', 'b'], ['a2', 'b', 'c']])).toStrictEqual(["a1", "a2"]);
+  expect(findDuplicates([['a1', 'b', 'b'], ['a2', 'b', 'c'], ['a1', 'b', 'b'], ['a2', 'b', 'c']]).sort()).toStrictEqual(["a1", "a2"]);
+  expect(findDuplicates([['a1', 'b', 'b'], ['a2', 'b', 'c'], ['a3', 'b', 'b'], ['a4', 'b', 'c']])).toStrictEqual([]);
+  expect(findDuplicates([])).toStrictEqual([]);
+  expect(findDuplicates(Array.from('abcdefghijklmnop'))).toStrictEqual([]);
+  expect(findDuplicates(Array.from('blablablablablabla')).sort()).toStrictEqual(["a", "b", "l"]);
 });
